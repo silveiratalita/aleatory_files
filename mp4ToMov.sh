e9fa44fab4ccd121d34c829c091546e1454464b1
Talita <silveira.tacristina@gmail.com>
2024-02-19 22:13:59 -0300

    mp4 videos to mov

---
+++ mp4ToMov.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for file in *.mp4; do
+  base_name=$(basename "$file" .mp4)
+  if [ -e "${base_name}.mov" ]; then
+    continue
+  fi
+    ffmpeg -i $base_name.mp4 -c:v dnxhd -profile:v dnxhr_hq -pix_fmt yuv422p -c:a pcm_s16le $base_name.mvo
+   echo " $file.mvo sucessfull created! "
+done
+
+
