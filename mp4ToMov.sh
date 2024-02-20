be07c5a9a1272e8e78fec35d97b29d2ff6a47cac
Talita <silveira.tacristina@gmail.com>
2024-02-20 20:54:46 -0300

    chore: permisão de escrita, leitura e execução do arquivo gerado

--- mp4ToMov.sh
+++ mp4ToMov.sh
@@ -5,6 +5,7 @@ for file in *.mp4; do
     continue
   fi
     ffmpeg -i $base_name.mp4 -c:v dnxhd -profile:v dnxhr_hq -pix_fmt yuv422p -c:a pcm_s16le $base_name.mvo
+    chmod 777 $base_name.mvo
    echo " $file.mvo sucessfull created! "
 done
 
